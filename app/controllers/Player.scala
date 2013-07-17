a936912c99bbab942e604cf5b9dd718eece1031d
Piyush Purang <ppurang@gmail.com>
2013-07-17 09:22:33 +0200

    make the uuid put work for a test to pass

--- app/controllers/Player.scala
+++ app/controllers/Player.scala
@@ -1,11 +1,18 @@
 package controllers
 
 import play.api.mvc._
+import java.util.UUID
 
 object Player extends Controller {
   
-  def register = Action {
-    Created("1").withHeaders("Location" -> "http://localhost:9000/player/1")
+  def register(uid: String) = Action {
+      try {
+        UUID.fromString(uid)
+        Created(s"$uid").withHeaders("Location" -> s"http://localhost:9000/player/$uid")
+      }
+      catch {
+        case e:IllegalArgumentException => BadRequest(s"$uid isn't a uuid")
+      }
   }
   
 }
