1dd92c157c23bd7ee238570d8692311a947db5df
Piyush Purang <piyush.purang@groupon.com>
2013-09-16 22:47:44 +0200

    play 2.1.4 + some dependencies + commented out atmos

--- project/Build.scala
+++ project/Build.scala
@@ -1,6 +1,7 @@
 import sbt._
 import Keys._
 import play.Project._
+//import com.typesafe.sbt.SbtAtmosPlay.atmosPlaySettings
 
 object ApplicationBuild extends Build {
 
@@ -10,12 +11,20 @@ object ApplicationBuild extends Build {
   val appDependencies = Seq(
     // Add your project dependencies here,
     jdbc,
-    anorm
+    anorm,
+    "org.scala-lang" %% "scala-pickling" % "0.8.0-SNAPSHOT",
+    "com.typesafe.slick" %% "slick" % "1.0.1",
+    "net.liftweb" %% "lift-json-scalaz7" % "2.5",
+    "com.stackmob" %% "newman" % "1.0.0",
+    "io.spray" % "spray-client" % "1.2-20130822"
   )
 
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
-  )
+    scalaVersion := "2.10.2",
+    resolvers += Resolver.sonatypeRepo("snapshots"),
+    resolvers += "spray nightly" at "http://nightlies.spray.io"
+    // Add your own project settings here
+  )//.settings((atmosPlaySettings): _*)      //https://groups.google.com/forum/#!topic/typesafe-console/RgvV-yf2Ce8
 
 }
