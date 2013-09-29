e7c0539adfa40fe1714b10ec4579c33d485eab7f
Piyush Purang <ppurang@gmail.com>
2013-09-30 01:11:34 +0200

    play config get returns options .. so fila fast if not defined

--- app/controllers/Player.scala
+++ app/controllers/Player.scala
@@ -7,7 +7,7 @@ import play.api.Play.current
 
 object Player extends Controller {
 
-  val server = current.configuration.getString("http.base.url")
+  val server = current.configuration.getString("http.base.url").get //fail fast
   
   def register(uid: String) = Action {
       try {
