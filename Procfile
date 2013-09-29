057e0bff86cb4bc2b09047cf92bc32b0fd1dbc26
Piyush Purang <ppurang@gmail.com>
2013-09-30 00:36:30 +0200

    heroku use production.conf

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: target/universal/stage/bin/extreme-play -Dhttp.port=$PORT
+web: target/universal/stage/bin/extreme-play -Dhttp.port=$PORT -Dconfig.resource=production.conf
--- app/controllers/Player.scala
+++ app/controllers/Player.scala
@@ -2,13 +2,17 @@ package controllers
 
 import play.api.mvc._
 import java.util.UUID
+import play.Play
+import play.api.Play.current
 
 object Player extends Controller {
+
+  val server = current.configuration.getString("http.base.url")
   
   def register(uid: String) = Action {
       try {
         UUID.fromString(uid)
-        Created(s"$uid").withHeaders("Location" -> s"http://localhost:9000/player/$uid")
+        Created(s"$uid").withHeaders("Location" -> s"$server/player/$uid")
       }
       catch {
         case e:IllegalArgumentException => BadRequest(s"$uid isn't a uuid")
