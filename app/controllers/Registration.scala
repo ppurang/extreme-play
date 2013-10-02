b906b0481fa339b60ad120ca3ce211a255437405
Daniel Jentsch <d.jentsch@fu-berlin.de>
2013-10-02 19:22:12 +0200

    Implement Registration and a Player Model

--- app/controllers/Registration.scala
+++ app/controllers/Registration.scala
@@ -4,12 +4,17 @@ import play.api.mvc._
 import play.api.libs.json._
 import models._
 import NewPlayer._
+
 object Registration extends Controller {
+
   def register = Action(parse.json) { request =>
-    request.body.validate[NewPlayer].map{
-      case NewPlayer(name, url) => Ok("Hello " + name + ", you're "+url)
-    }.recoverTotal{
-      e => BadRequest("Faulty payload: "+ JsError.toFlatJson(e))
+    request.body.validate[NewPlayer].map {
+      case newPlayer: NewPlayer =>
+        val player = models.Player.register(newPlayer)
+        import player._
+        Ok(s"Hello $name, you're $url")
+    }.recoverTotal {
+      e => BadRequest("Faulty payload: " + JsError.toFlatJson(e))
     }
   }
 
