f1be3765857539873aa98e560bd11130af005700
Daniel Jentsch <d.jentsch@fu-berlin.de>
2013-10-02 20:18:51 +0200

    Player registration now returns player uid

--- app/controllers/Registration.scala
+++ app/controllers/Registration.scala
@@ -7,13 +7,17 @@ import NewPlayer._
 
 object Registration extends Controller {
 
-  def register = Action(parse.json) { request =>
+  def register = Action(parse.json) { implicit request =>
     request.body.validate[NewPlayer].map {
       case newPlayer: NewPlayer =>
         val attemp = models.Player.register(newPlayer)
         attemp.map { player =>
           import player._
-          Ok(s"Hello $name, you're $url")
+          Created(s"Hello $name, you're $url").
+          withHeaders(
+            "location" ->
+              routes.History.player(uid.toString).absoluteURL().toString
+          )
         }.getOrElse {
           Conflict("Name or URL already in use.")
         }
