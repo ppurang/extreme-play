d32c9460723411f9f7c045872f3f64fff70c6149
George Leontiev <folone@gmail.com>
2013-10-02 19:51:24 +0200

    Sum task implementation.

--- app/controllers/logic/package.scala
+++ app/controllers/logic/package.scala
@@ -2,6 +2,9 @@ package controllers
 
 import models.Task
 import com.nicta.rng._
+import scalaz._
+import std.anyVal._
+import syntax.foldable._
 
 package object logic {
 
@@ -10,7 +13,7 @@ package object logic {
   ))
 
   val infiniteTaskRepo: TasksRepo =
-    new InfiniteTaskRepo(infiniteTaskStream)
+    new InfiniteTaskRepo(infiniteRandomTaskStream)
 
   import Rng._
   val simpleRandomTaskGen: Rng[Task] =
@@ -18,7 +21,26 @@ package object logic {
       query ← propernounstring(Size(10))
     } yield Task(query, _ ⇒ true)
 
-  val infiniteTaskStream = Iterator.continually {
-    simpleRandomTaskGen.list1(Size(100)).run.unsafePerformIO.list
-  }.flatten
+  val simpleSumTaskGen: Rng[Task] =
+    for {
+      n ← chooseint(1, 10)
+      m ← chooseint(1, 10)
+    } yield Task(s"What is the sum of $n and $m?", _ == (n + m).toString)
+
+  val generalizedSumTaskGen: Rng[Task] =
+    for {
+      n ← chooseint(1,10)
+      lst ← chooseint(1,10).list1(Size(10))
+    } yield Task(s"What is the sum of $n + ${lst.list.mkString(" + ")}",
+                    _ == (n + lst.suml).toString)
+
+  val infiniteRandomTaskStream = getTaskStream(simpleRandomTaskGen)
+  val infiniteSumTaskGen = getTaskStream(simpleSumTaskGen)
+  val infiniteGeneralizedSumTaskStream =
+    getTaskStream(generalizedSumTaskGen)
+
+  def getTaskStream(gen: Rng[Task]) =
+    Iterator.continually {
+      gen.list1(Size(100)).run.unsafePerformIO.list
+    }.flatten
 }
