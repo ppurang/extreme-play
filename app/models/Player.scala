53609083661c4f203cc8cb350955c3c5d29e0804
Daniel Jentsch <d.jentsch@fu-berlin.de>
2013-10-02 19:52:58 +0200

    Add detection of duplicated player names or urls

--- app/models/Player.scala
+++ app/models/Player.scala
@@ -1,21 +1,25 @@
 package models
 
 import concurrent.stm._
+import scala.util.Try
 
 case class Player(name: String, url: String)
 
 object Player {
   private val ref = Ref(Seq[Player]()).single
 
-  def register(newPlayer: NewPlayer): Player = {
-    val NewPlayer(newName, url) = newPlayer
-    val player = Player(newName, url)
-    
-    ref.transform{
-      _.filter(_.name != newName) :+ player
+  def register(newPlayer: NewPlayer): Try[Player] = {
+    val NewPlayer(name, url) = newPlayer
+    val player = Player(name, url)
+
+    Try {
+      ref.transform { players =>
+        assert(players forall (_.name != name))
+        assert(players forall (_.url != url))
+        players :+ player
+      }
+      player
     }
-    
-    player
   }
 
   def all: Seq[Player] = ref.get
