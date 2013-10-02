b87e043ddbd12e49629951961f81cc325a97ba80
oheidemann <heidemann@gmail.com>
2013-10-02 21:47:23 +0200

    add get all player to do the tests

--- app/controllers/Registration.scala
+++ app/controllers/Registration.scala
@@ -6,6 +6,15 @@ import models._
 import NewPlayer._
 
 object Registration extends Controller {
+  implicit val writePlayerPublic = new Writes[models.Player] {
+    def writes(v: models.Player): JsValue = {
+      Json.obj("uid" -> v.uid.toString(), "name" -> v.name, "url" -> v.url)
+    }
+  }
+  
+  def findAll = Action(parse.empty) { implicit req => 
+    Ok(Json.toJson(models.Player.all))
+  }
 
   def register = Action(parse.json) { implicit request =>
     request.body.validate[NewPlayer].map {
