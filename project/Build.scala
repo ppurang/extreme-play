349ea39c8461d233bf1590c8295cc5e166eea2be
Piyush Purang <ppurang@gmail.com>
2014-01-15 00:59:02 +0100

    play 2.2.1 + scalaz 7.0.5 + nicta rng 1.1 + sbt 0.13.1

--- project/Build.scala
+++ project/Build.scala
@@ -13,6 +13,7 @@ object ApplicationBuild extends Build {
     jdbc,
     anorm,
     filters,
+    "org.scalaz" %% "scalaz-core" % "7.0.5",
     "org.scala-lang" %% "scala-pickling" % "0.8.0-SNAPSHOT",
     "com.typesafe.slick" %% "slick" % "1.0.1",
     "net.liftweb" %% "lift-json-scalaz7" % "2.5",
@@ -20,7 +21,7 @@ object ApplicationBuild extends Build {
     "com.typesafe.akka" %% "akka-testkit" % "2.2.1" % "test",
     "org.scalatest" % "scalatest_2.10.0" % "1.8" % "test",
     "io.spray" % "spray-client" % "1.2-20130822",
-    "com.nicta" %% "rng" % "1.0-SNAPSHOT"
+    "com.nicta" %% "rng" % "1.1"
   )
 
 
