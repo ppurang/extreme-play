7e7d2b9ad5a4d703fcfffdb911de6f6f4059c8f9
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2014-01-28 21:03:35 +0100

    fix the world

--- app/Global.scala
+++ app/Global.scala
@@ -6,7 +6,6 @@ import lib.game.Scorer
 
 object Global extends WithFilters(new GzipFilter()) with GlobalSettings {
   override def onStart(app: Application) {
-  	val scorer = Akka.system.actorOf(Props[Scorer])
     Logger.info("Application has started")
   }  
   
