29c316c61a61b136cbea1c992c9794abd6a18bfe
Daniel Westheide <daniel.westheide@innoq.com>
2014-01-28 20:14:07 +0100

    Implement History board actor

---
+++ app/lib/game/History.scala
@@ -0,0 +1,32 @@
+package lib.game
+
+import akka.actor.Actor
+import lib.game.History.{HistoryResponse, GetHistory, HistoryEvent}
+
+object History {
+  sealed trait HistoryEvent {
+    def playerId: String
+  }
+  case class GetHistory(playerId: String)
+  case class HistoryResponse(playerId: String, history: Vector[HistoryEvent])
+}
+class History extends Actor {
+
+  context.system.eventStream.subscribe(self, classOf[HistoryEvent])
+
+  def receive = updated(Map.empty[String, Vector[HistoryEvent]])
+
+  private def updated(state: Map[String, Vector[HistoryEvent]]): Receive = {
+    case GetHistory(playerId) =>
+      sender ! HistoryResponse(playerId, state.getOrElse(playerId, Vector.empty))
+    case e: HistoryEvent =>
+      val newState = if (state.keySet(e.playerId)) {
+        state map { case (k, v) => if (k == e.playerId) (k, v :+ e) else (k, v)}
+      }
+      else {
+        state updated(e.playerId, Vector(e))
+      }
+      context become updated(newState)
+  }
+
+}
