4ce75c470a86fe3751c5b21a1fd3aa28516470bf
Gerrit <gerrit_gruben@gmail.com>
2014-01-28 20:30:47 +0100

    - make compilation work - Scorer subscribes to EventStream now

--- app/lib/game/Score.scala
+++ app/lib/game/Score.scala
@@ -1,11 +1,17 @@
 package lib.game
 
 import akka.actor.Actor
+import akka.event.LoggingReceive
 
 
-sealed trait ScoreEvents;
-case class ScoreChange(playerUUID: String, scoreChange: Int) extends ScoreEvents;
+sealed trait ScoreEvents
+case class ScoreChange(playerUUID: String, scoreChange: Int) extends ScoreEvents
 
 class Scorer extends Actor {
+   val listenToEvents = Set(classOf[PlayerProtocol.TaskAnswered], classOf[PlayerProtocol.TaskAnswerFailed])
+   listenToEvents.map{ clss => context.system.eventStream subscribe (self,clss) }
   
+   override def receive = LoggingReceive {
+     case _ => 
+   }
 }
