e6bc34e22f4d7645135eb69a7e52e14473cee351
Piyush Purang <ppurang@gmail.com>
2014-01-29 03:08:11 +0100

    remove tabs + add a todo

--- app/lib/game/Score.scala
+++ app/lib/game/Score.scala
@@ -8,33 +8,37 @@ import talk._
 import models.Task
 
 sealed trait ScoreEvents
-case class ScoreChangeAnswered(task : TaskAnswered, playerUUID: String, scoreChange: Int, timestamp : Long = System.currentTimeMillis()) extends ScoreEvents
-case class ScoreChangeFailed(task : TaskAnswerFailed, playerUUID: String, scoreChange: Int, timestamp : Long = System.currentTimeMillis()) extends ScoreEvents
+
+case class ScoreChangeAnswered(task: TaskAnswered, playerUUID: String, scoreChange: Int, timestamp: Long = System.currentTimeMillis()) extends ScoreEvents
+
+case class ScoreChangeFailed(task: TaskAnswerFailed, playerUUID: String, scoreChange: Int, timestamp: Long = System.currentTimeMillis()) extends ScoreEvents
 
 class Scorer extends Actor {
-   val listenToEvents = Set(classOf[PlayerProtocol.TaskAnswered], classOf[PlayerProtocol.TaskAnswerFailed])
-   listenToEvents.map{ clss => context.system.eventStream subscribe (self,clss) }
-  
-   override def receive = LoggingReceive {
-    case m @ TaskAnswered(teamName: String, answer: Answer, task: Task) => handleTaskAnswered(teamName, answer, task, m)
-    case m @ TaskAnswerFailed(teamName: String, error: Error, task: Task) => handleTaskAnsweredFailed(teamName, error, task, m)
-   }
+  val listenToEvents = Set(classOf[PlayerProtocol.TaskAnswered], classOf[PlayerProtocol.TaskAnswerFailed])
+  listenToEvents.map {
+    clss => context.system.eventStream subscribe(self, clss)
+  }
 
-   def handleTaskAnswered(teamName: String, answer: Answer, task: Task, taskAnswered : TaskAnswered) = {
-   	val score = verifyAnswer(answer, task)
-   	val eventScore = ScoreChangeAnswered(taskAnswered, teamName, score)
-   	context.system.eventStream publish eventScore
-   }
+  override def receive = LoggingReceive {
+    case m@TaskAnswered(teamName: String, answer: Answer, task: Task) => handleTaskAnswered(teamName, answer, task, m)
+    case m@TaskAnswerFailed(teamName: String, error: Error, task: Task) => handleTaskAnsweredFailed(teamName, error, task, m)
+  }
 
-   def handleTaskAnsweredFailed(teamName: String, error: Error, task: Task, taskAnsweredFailed : TaskAnswerFailed) = {
-   	val eventScore = ScoreChangeFailed(taskAnsweredFailed, teamName, Scorer.PENALTY_SCORE)
-   	context.system.eventStream publish eventScore
-   }
+  def handleTaskAnswered(teamName: String, answer: Answer, task: Task, taskAnswered: TaskAnswered) = {
+    val score = verifyAnswer(answer, task)
+    val eventScore = ScoreChangeAnswered(taskAnswered, teamName, score)
+    context.system.eventStream publish eventScore
+  }
 
-   def verifyAnswer(answer: Answer, task: Task) : Integer = task.scoreAnswer(answer.text)
-   
+  def handleTaskAnsweredFailed(teamName: String, error: Error, task: Task, taskAnsweredFailed: TaskAnswerFailed) = {
+    val eventScore = ScoreChangeFailed(taskAnsweredFailed, teamName, Scorer.PENALTY_SCORE)
+    context.system.eventStream publish eventScore
+  }
+
+  def verifyAnswer(answer: Answer, task: Task): Integer = task.scoreAnswer(answer.text)
+
 }
 
 object Scorer {
-	val PENALTY_SCORE = -100
+  val PENALTY_SCORE = -100 //todo this should be -SCORE
 }
